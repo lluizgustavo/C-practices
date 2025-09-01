1663bf90fd12e1c23534fcd06f5743c34dc24825
Gustavo <luizsilvagf98@gmail.com>
2025-09-01 15:41:29 -0300

    Código criado na língua inglesa, com o foco em manter o meu inglês aifado. Nesse código em C, o usuário digita seu nome, idade e altura, as 3 informações serão armazenadas em variáveis já declaradas anteriormente e, feito isso, será imprimido na tela todas as informações que o usuário digitou

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define txt "My first code in C"
+
+int main()
+{
+ char text [50]= "";
+ float height = 0.0;
+ int age = 0;
+
+printf("%s,\n", txt);
+printf("-=-=-=-=-=-=-\n");
+ printf("Type here your name: ");
+ scanf("%s.\n", &text);
+
+ printf("And here, type your age: ");
+ scanf("%d.\n", &age);
+
+ printf("And finally, type here you height: ");
+ scanf("%f\n", &height);
+
+ printf("Okay.... Lets see. Your name is %s, you is %d years old and your height its %.2f", text, age, height);
+}
