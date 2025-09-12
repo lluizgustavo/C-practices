721479bebeba4f28fcd7406a203eeece3523966d
Luiz Gustavo <luizsilvagf98@gmail.com>
2025-09-12 18:43:39 -0300

    feat

--- main.c
+++ main.c
@@ -4,20 +4,20 @@
 
 int main()
 {
- char text [50]= "";
- float height = 0.0;
- int age = 0;
+    char text[50] = "";
+    float height = 0.0;
+    int age = 0;
 
-printf("%s,\n", txt);
-printf("-=-=-=-=-=-=-\n");
- printf("Type here your name: ");
- scanf("%s.\n", &text);
+    printf("%s,\n", txt);
+    printf("-=-=-=-=-=-=-\n");
+    printf("Type here your name: ");
+    scanf("%s.\n", &text);
 
- printf("And here, type your age: ");
- scanf("%d.\n", &age);
+    printf("And here, type your age: ");
+    scanf("%d.\n", &age);
 
- printf("And finally, type here you height: ");
- scanf("%f\n", &height);
+    printf("And finally, type here you height: ");
+    scanf("%f\n", &height);
 
- printf("Okay.... Lets see. Your name is %s, you is %d years old and your height its %.2f", text, age, height);
+    printf("Okay.... Lets see. Your name is %s, you is %d years old and your height its %.2f", text, age, height);
 }
